0969a532c058639a5daaa67e0176ccb130a5d980
NgocTruong0701 <ngoctruongcv@gmail.com>
2023-02-25 11:42:55 +0700

    Update 11:42 25/02/2023

--- BTL_JAVA_KyXuan/src/main/java/controller/EventController.java
+++ BTL_JAVA_KyXuan/src/main/java/controller/EventController.java
@@ -4,10 +4,48 @@
  */
 package controller;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import model.Event;
+
 /**
  *
  * @author linhc
  */
 public class EventController {
+    // 1 đối tượng Event nhằm chứa dữ liệu Event để sử lý
+    private Event event = new Event();
+    // 1 đối tượng FileController để ta có thể thao tác với tệp
+    private FileController fileController = new FileController();
+
+    // Các phương thức khởi tạo có đối và không đối
+    public EventController() {
+    }
+    
+    public EventController(Event event, FileController fileController){
+        this.event = event;
+        this.fileController = fileController;
+    }
+    
+    // Method get Events từ File 
+    public List<Event> readEventsFromFile(String fileName) throws IOException, ParseException{
+        fileController.OpenFileToRead(fileName);
+        List<Event> listEvents = new ArrayList<>();
+        // Định nghĩa chuỗi qua đối tượng Date
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        while(fileController.scanner.hasNext()){
+            String data = fileController.scanner.nextLine();
+            String[] arrData = data.split("\\|");
+            listEvents.add(new Event(Long.parseLong(arrData[0]), arrData[1], format.parse(arrData[2]),format.parse(arrData[3]), Integer.parseInt(arrData[4]), arrData[5]));
+        }
+        
+        fileController.CloseFileAfterRead(fileName);
+        
+        return listEvents;
+    }
     
 }
